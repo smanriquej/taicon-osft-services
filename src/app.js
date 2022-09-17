a941970bd7eec7369249986bd7118eacf72c7658
Santiago Manrique Jimenez <santiagomanrique@codiscos.com>
2022-09-17 00:38:55 -0500

    - Add indices in route

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, 'images')));
 app.use('/', require('./routes/root'));
-app.use('/indicesFilters', require('../src/routes/indicesfilters'));
+//app.use('/indicesFilters', require('../src/routes/indicesfilters'));
 app.use('/auth', require('./routes/auth'));
 
 // app.all('*', (req, res) => {
@@ -50,7 +50,7 @@ app.use('/auth', require('./routes/auth'));
 // });
 
 // app.use('/.netlify/functions/indices', indicesRoutes);
-//app.use('/indices', indicesRoutes);
+app.use('/indices', indicesRoutes);
 // app.use('/.netlify/functions/indicesFilter', indicesFilterRoutes);
 //app.use('/indicesFilter', indicesFilterRoutes);
 // app.use('/.netlify/functions/', authRoutes);
