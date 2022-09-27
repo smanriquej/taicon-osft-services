407dc83441990ab426b2c9b3868251c479b34e33
Santiago Manrique Jimenez <santiagomanrique@codiscos.com>
2022-09-26 23:01:49 -0500

    Rever changes configuration politicy

--- src/app.js
+++ src/app.js
@@ -16,18 +16,13 @@ app.use(bodyParser.json());
 app.use('/images', express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
+  // Set CORS headers so that the React SPA is able to communicate with this server
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PUT,PATCH,DELETE,OPTIONS'
   );
-  if (req.method === "OPTIONS") {
-    res.header(
-      "Access-Control-Allow-Methods",
-      'GET,POST,PUT,PATCH,DELETE,OPTIONS'
-    )
-    return res.status(200).json({})
-  };
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
